16e4ae8dca9c48aca5f21727973a6a060b457329
timothy-coder <nicopar666xd@gmail.com>
2023-02-16 11:21:32 -0500

    Update index.jsx

--- resources/js/Pages/Trabajador/index.jsx
+++ resources/js/Pages/Trabajador/index.jsx
@@ -9,9 +9,9 @@ export default function Trabajadores(props) {
         >
             <Head title="Trabajadores" />
 
-            <main className="w-full px-6 overflow-y-auto">
-			<div className="flex items-center justify-between gap-4 flex-wrap">
-				<div className="">
+            <main className="w-full bg-white overflow-y-auto">
+			<div className="flex items-center justify-between gap-4 flex-wrap bg-black">
+				<div className="px-6">
 					<h1 className="text-4xl font-semibold mb-5 text-lime-700">Dashboard</h1>
 					<ul className="flex items-center gap-4">
 						<li>
@@ -30,7 +30,7 @@ export default function Trabajadores(props) {
 				</a>
 			</div>
 
-			<ul className="grid gap-6 mt-9 grid-cols-[repeat(auto-fit,minmax(240px,1fr))]">
+			<ul className="px-6 grid gap-6 mt-9 grid-cols-[repeat(auto-fit,minmax(240px,1fr))]">
 				<li className="flex item-center gap-6 p-6 rounded-[20px] bg-blue-400">
 					<i className="w-20 h-20 text-4xl flex justify-center item-center rounded-[10px] bg-green-400" ></i>
 					<span class="text">
@@ -55,7 +55,7 @@ export default function Trabajadores(props) {
 			</ul>
 
 
-			<div className="flex wrap gap-6 w-full mt-6">
+			<div className="px-6 flex wrap gap-6 w-full mt-6">
 				<div className="grow basis-[500px]">
 					<div className="flex items-center gap-4 mb-6">
 						<h3 className="text-2xl font-semibold mr-auto">Entrada recientes</h3>
@@ -118,31 +118,7 @@ export default function Trabajadores(props) {
 						</tbody>
 					</table>
 				</div>
-				<div className="grow basis-[300px]">
-					<div className="flex">
-						<h3>Todas las entradas</h3>
-						<i className="items-end" >+</i>
-						<i className="items-end" >filtro</i>
-					</div>
-					<ul className="w-full">
-						<li className="w-full flex justify-between items-center mb-4 
-						px-5 py-3.5 rounded-[10px] bg-gray-200">
-							<p>logistica</p>
-						</li>
-						<li className="border-l-[10px] border-l-[blue] border-solid">
-							<p>oti</p>
-						</li>
-						<li className="border-l-[10px] border-l-[orange] border-solid">
-							<p>rectorado</p>
-						</li>
-						<li class="border-l-[10px] border-l-[blue] border-solid">
-							<p>oti</p>
-						</li>
-						<li className="border-l-[10px] border-l-[orange] border-solid">
-							<p>oti</p>
-						</li>
-					</ul>
-				</div>
+				
 			</div>
 		</main>
         </AuthenticatedLayout>
