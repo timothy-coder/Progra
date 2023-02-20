560e5db5549ce6c15bcfa30a04eebb075d11142b
timothy-coder <nicopar666xd@gmail.com>
2023-02-20 10:19:59 -0500

    sdfg

--- resources/js/Pages/Config/index.jsx
+++ resources/js/Pages/Config/index.jsx
@@ -78,7 +78,7 @@ export default function Configuración(props) {
 								<td className="px-0 py-1">d_cargo@uncp.edu.pe</td>
 								<td className="px-0 py-4">
                                 <FontAwesomeIcon icon={faWrench} className="text-2.5 text-white 
-								font-bold px-4 py-1.5 rounded-[20px] bg-black hover:bg-black"/>
+								font-bold px-4 py-1.5 rounded-[20px] bg-black hover:bg-black" />
                                 </td>
 							</tr>
 							<tr className="bg-white hover:bg-gray-200">
--- routes/web.php
+++ routes/web.php
@@ -40,8 +40,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-
-
+    
     Route::get('/reportes',[ReporteController::class,'index'])->name('reporte.index');
     Route::get('/trabajadores',[TrabajadorController::class,'index'])->name('trabajador.index');
     Route::get('/usuarios',[UserController::class,'index'])->name('user.index');
