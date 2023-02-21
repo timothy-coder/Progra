ca766e1af73a79b6a43d6b918546081c9940b02f
timothy-coder <nicopar666xd@gmail.com>
2023-02-21 09:46:10 -0500

    as

--- resources/js/Pages/Dependencia/index.jsx
+++ resources/js/Pages/Dependencia/index.jsx
@@ -24,17 +24,18 @@ export default function Dependencia(props) {
 						</li>
 					</ul>
 				</div>
-				<a href="#" className="pr-6 ">
-                    
-					<span className="h-9 flex justify-center items-center
-					 font-medium gap-2.5 bg-black text-white px-4 py-0 rounded-lg hover:text-[rgb(151,115,61)] "><FontAwesomeIcon icon={faDownload} />Descargar reporte</span>
-				</a>
+				
 			</div>
 
 			<div className="px-6 flex wrap gap-6 w-full mt-6">
 				<div className="grow basis-[500px]">
 					<div className="flex items-center gap-4 mb-6">
 						<h3 className="text-2xl font-semibold mr-auto">Lista de Dependencias</h3>
+						<a href="#" className="pr-6 ">
+                    
+					<span className="h-9 flex justify-center items-center
+					 font-medium gap-2.5 bg-black text-white px-4 py-0 rounded-lg hover:text-[rgb(151,115,61)] "><FontAwesomeIcon icon={faDownload} />Descargar reporte</span>
+				</a>
 					</div>
 					<table className="w-full border-collapse">
 						<thead className="text-[13px] text-left pb-3 border-b-[black] border-b border-solid">
