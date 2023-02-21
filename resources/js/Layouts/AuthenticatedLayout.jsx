ffdf26adcea06182f15a0e4bb40a0af54365be6c
timothy-coder <nicopar666xd@gmail.com>
2023-02-21 12:46:03 -0500

    Update AuthenticatedLayout.jsx

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -13,7 +13,7 @@ export default function Authenticated({ auth, header, children }) {
             <nav className="bg-[rgb(62,62,62)] h-[100%] border-b border-[rgb(62,62,62)] fixed ">
                 <div className=" px-4 sm:px-6 lg:px-0">
                     <div className="justify-between space-y-4  w-[10rem]">
-                        <div className="shrink-0 flex items-center p-3.5 rounded-b-xl">
+                        <div className="shrink-0 flex items-center p-3.5 ">
                             <Link href={route('reporte.index')}>
                                 {/* href='/' cambiar*/}
                                 <ApplicationLogo className="block h-[5rem] w-auto fill-current " />
