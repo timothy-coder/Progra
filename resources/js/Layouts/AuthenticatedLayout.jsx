ff80e8b278802950b41a00f864d0f99c9ed18a12
InFeRnO-06032020 <gsanabriachanca@gmail.com>
2023-02-21 13:05:43 -0500

    f

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -23,8 +23,8 @@ export default function Authenticated({ auth, header, children }) {
                             </Link>
                         </div>
                         
-                        <div className="flex flex-col space-x-3  ">
-                            <div className="max-sm:hidden space-y-8 sm:-my-px sm:mx-1 ">
+                        <div className="flex flex-col space-x-3 ">
+                            <div className="max-sm:hidden space-y-8 sm:-my-px sm:mx-1">
                                 <NavLink href={route('reporte.index')} active={route().current('reporte.index')}>
                                 <FontAwesomeIcon icon={faBoxArchive} /><a className="px-2">Reportes</a>
                                 </NavLink>
