76ef17fd00023be3818421717a5390b66d2d8cf2
timothy-coder <nicopar666xd@gmail.com>
2023-02-23 11:27:07 -0500

    Update Edit.jsx

--- resources/js/Pages/Profileuser/Edit.jsx
+++ resources/js/Pages/Profileuser/Edit.jsx
@@ -21,11 +21,6 @@ export default function Edit({ auth, mustVerifyEmail, status }) {
                             className="max-w-xl"
                         />
                     </div>
-
-                    <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                        <UpdatePasswordForm className="max-w-xl" />
-                    </div>
-
                     <div className="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                         <DeleteUserForm className="max-w-xl" />
                     </div>
