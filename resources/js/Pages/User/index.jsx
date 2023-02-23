693f8361ad76d8b85fd42d4964a0d18403238fad
timothy-coder <nicopar666xd@gmail.com>
2023-02-23 11:58:38 -0500

    a

--- resources/js/Pages/User/index.jsx
+++ resources/js/Pages/User/index.jsx
@@ -25,7 +25,7 @@ export default function Dependencia(props) {
 				</a>
 					</div>
 					<table className="w-full border-collapse h-100 break-all text-center">
-						<thead className="text-[13px] text-left pb-3 border-b-[black] border-b border-solid text-center">
+						<thead className="text-[13px]  pb-3 border-b-[black] border-b border-solid text-center">
 							<tr>
 								<th>Correo</th>
                                 <th>Usuario</th>
