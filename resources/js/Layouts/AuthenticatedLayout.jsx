41174c8f05065a82f0a71542038a3178fb878123
timothy-coder <nicopar666xd@gmail.com>
2023-02-24 11:31:33 -0500

    Update AuthenticatedLayout.jsx

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -14,7 +14,7 @@ export default function Authenticated({ auth, children }) {
     
     return (
         <div className="min-w-screen bg-white ">
-            <nav className=" border-b border-[rgb(62,62,62)] bg-[rgb(62,62,62)]">
+            <nav className="sm:pt-0 pt-2 border-b border-[rgb(62,62,62)] bg-[rgb(62,62,62)]">
                 <div className="px-4 sm:px-1 lg:px-0 w-[10rem] bg-[rgb(62,62,62)]  h-[100%]">
                     <div className="px-4 sm:px-1 lg:px-0 w-[10rem] sm:fixed bg-[rgb(62,62,62)] h-[100%]">
                     <div className="justify-between space-y-4">
